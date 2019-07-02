d6218e1176c2288c7ab266750344c8607129bc21
Ying Zhou <ybzhou@thoughtworks.com>
2019-07-02 10:31:53 +0800

    remove query and transfer code

--- chaincode/java/src/main/java/com/thoughtworks/fabric/workshop/AccountBasedChaincode.java
+++ chaincode/java/src/main/java/com/thoughtworks/fabric/workshop/AccountBasedChaincode.java
@@ -26,78 +26,8 @@ public class AccountBasedChaincode extends ChaincodeBase {
         switch (func) {
             case "createWallet":
                 return createWallet(stub, parameters);
-            case "getWallet":
-                return getWallet(stub, parameters);
-            case "transfer":
-                return transfer(stub, parameters);
             default:
                 return newErrorResponse(error("Unsupported method", ""));
-        }
-    }
-
-    private Response transfer(ChaincodeStub stub, List<String> parameters) {
-        if (parameters.size() != 3)
-            return newErrorResponse(error("Incorrect number of arguments, expecting 3", ""));
-        final String fromWalletId = parameters.get(0);
-        final String toWalletId = parameters.get(1);
-        final String tokenAmount = parameters.get(2);
-        if (StringUtils.isEmpty(fromWalletId) || StringUtils.isEmpty(toWalletId) || StringUtils.isEmpty(tokenAmount))
-            return newErrorResponse(error("Invalid argument(s)", ""));
-        if (fromWalletId.equals(toWalletId))
-            return newErrorResponse(error("From-wallet is same as to-wallet", ""));
-
-        double tokenAmountDouble;
-        try {
-            tokenAmountDouble = Double.parseDouble(tokenAmount);
-            if (tokenAmountDouble < 0d) {
-                return newErrorResponse(error("Invalid token amount", ""));
-            }
-        } catch (NumberFormatException e) {
-            return newErrorResponse(error("Token amount parse error", ""));
-        }
-
-        try {
-            String fromWalletString = stub.getStringState(fromWalletId);
-            if (StringUtils.isEmpty(fromWalletString))
-                return newErrorResponse(error("Nonexistent from-wallet", ""));
-            String toWalletString = stub.getStringState(toWalletId);
-            if (StringUtils.isEmpty(toWalletString))
-                return newErrorResponse(error("Nonexistent to-wallet", ""));
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            Wallet fromWallet = objectMapper.readValue(fromWalletString, Wallet.class);
-            Wallet toWallet = objectMapper.readValue(toWalletString, Wallet.class);
-
-            if (fromWallet.getTokenAmount() < tokenAmountDouble)
-                return newErrorResponse(error("Token amount not enough", ""));
-
-            fromWallet.setTokenAmount(fromWallet.getTokenAmount() - tokenAmountDouble);
-            toWallet.setTokenAmount(toWallet.getTokenAmount() + tokenAmountDouble);
-            stub.putState(fromWalletId, objectMapper.writeValueAsBytes(fromWallet));
-            stub.putState(toWalletId, objectMapper.writeValueAsBytes(toWallet));
-
-            return newSuccessResponse(success("Transferred success"));
-        } catch (Throwable e) {
-            return newErrorResponse(error(e.getMessage(), ""));
-        }
-    }
-
-    private Response getWallet(ChaincodeStub stub, List<String> parameters) {
-        if (parameters.size() != 1) {
-            return newErrorResponse(error("Incorrect number of parameters, expected 1", ""));
-        }
-        final String walletId = parameters.get(0);
-        if (StringUtils.isEmpty(walletId)) {
-            return newErrorResponse(error("Invalid argument", ""));
-        }
-        try {
-            final String walletString = stub.getStringState(walletId);
-            if (StringUtils.isEmpty(walletString)) {
-                return newErrorResponse(error("Invalid argument", ""));
-            }
-            return newSuccessResponse(new ObjectMapper().writeValueAsBytes(success(walletString)));
-        } catch (JsonProcessingException e) {
-            return newErrorResponse(error(e.getMessage(), ""));
         }
     }
 
